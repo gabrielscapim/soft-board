082991df238fe8a68b206620145edbe3358f38c1
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-20 23:24:38 -0300

    Create flex component type

---
+++ src/types/flex-components/flex-component-type.ts
@@ -0,0 +1,14 @@
+export type RectangleFlexComponentType = {
+  name: 'Rectangle'
+  type: 'rectangle'
+  properties: {
+    x: number
+		y: number
+		width: number
+		height: number
+    rx: number
+    ry: number
+  }
+}
+
+export type FlexComponentType = RectangleFlexComponentType
