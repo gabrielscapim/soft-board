61089fbff50ef4e0a51cebaad533fd170d2c518e
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-21 00:50:19 -0300

    Fix drawer title

--- src/components/ComponentsDrawer/ComponentsDrawer.tsx
+++ src/components/ComponentsDrawer/ComponentsDrawer.tsx
@@ -8,9 +8,7 @@ export function ComponentsDrawer () {
       <input type="checkbox" className="drawer-toggle" />
       <div className="drawer-side">
         <ul className="menu bg-base-200 text-base-content min-h-full w-64 p-4">
-          <h1 className="text-sm font-semibold mb-2">
-            Components
-          </h1>
+          <li className="menu-title">Components</li>
           {Object.entries(FLEX_COMPONENT_NAMES).map(component => {
             const type = component[0] as FlexComponentType
             const name = component[1]
