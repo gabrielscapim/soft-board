d1ade632f68b0cf89e418539cf1e735acec627c4
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-21 21:01:33 -0300

    Create selected flex component state

--- src/lib/draggable-board/draggable-board.ts
+++ src/lib/draggable-board/draggable-board.ts
@@ -1,20 +1,26 @@
 import { Offset } from '../../types'
+import { UUID } from '../../types/common/uuid'
+import { BoardManager } from '../board-manager'
+import { BoardState } from '../board-state'
 
 /**
  * Class responsible for leaving SVG components inside the board draggable.
  */
 export class DraggableBoard {
-  private _flexBoard: SVGSVGElement
+  private _boardManager: BoardManager
+  private _flexBoardElement: SVGSVGElement
   private _grid: number
   private _offset: Offset | undefined
-  private _selectedFlexComponent: SVGGElement | undefined
+  private _selectedFlexElement: SVGGElement | undefined
   private _transform: DOMMatrix | undefined
 
   constructor (
-    flexBoard: SVGSVGElement,
+    boardState: BoardState,
+    flexBoardElement: SVGSVGElement,
     grid: number = 10
   ) {
-    this._flexBoard = flexBoard
+    this._boardManager = new BoardManager(boardState)
+    this._flexBoardElement = flexBoardElement
     this._grid = grid
 
     this.startDrag = this.startDrag.bind(this)
@@ -23,7 +29,7 @@ export class DraggableBoard {
   }
 
   private getMousePosition (event: MouseEvent) {
-    const screenCTM = this._flexBoard.getScreenCTM()
+    const screenCTM = this._flexBoardElement.getScreenCTM()
     const x = screenCTM ? (event.clientX - screenCTM.e) / screenCTM.a : 0
     const y = screenCTM ? (event.clientY - screenCTM.f) / screenCTM.d : 0
 
@@ -31,11 +37,11 @@ export class DraggableBoard {
   }
 
   public endDrag () {
-    this._selectedFlexComponent = undefined
+    this._selectedFlexElement = undefined
   }
 
   public onDragging (event: MouseEvent) {
-    if (this._selectedFlexComponent && this._offset) {
+    if (this._selectedFlexElement && this._offset) {
       event.preventDefault()
 
       const coord = this.getMousePosition(event)
@@ -44,24 +50,32 @@ export class DraggableBoard {
       const roundedDeltaX = Math.round(deltaX / this._grid) * this._grid
       const roundedDeltaY = Math.round(deltaY / this._grid) * this._grid
 
-      this._selectedFlexComponent.setAttribute('transform', `translate(${roundedDeltaX}, ${roundedDeltaY})`)
+      this._selectedFlexElement.setAttribute('transform', `translate(${roundedDeltaX}, ${roundedDeltaY})`)
     }
   }
 
   public startDrag (event: MouseEvent) {
     const target = event.target as SVGGElement
+    const draggableGroupElement = target.closest('.draggable-group') as SVGGElement | null
 
-    if (target.closest('#draggable-group')) {
-      this._selectedFlexComponent = target.closest('#draggable-group') as SVGGElement
+    if (draggableGroupElement) {
+      this._selectedFlexElement = draggableGroupElement
       this._offset = this.getMousePosition(event)
-      this._transform = this._selectedFlexComponent.transform.baseVal.consolidate()?.matrix
+      this._transform = this._selectedFlexElement.transform.baseVal.consolidate()?.matrix
+      const selectedFlexComponentId = this._selectedFlexElement.id
 
-      this._flexBoard.appendChild(this._selectedFlexComponent) // Move element to the first position
+      this._boardManager.onDragFlexComponent(selectedFlexComponentId as UUID)
+
+      this._flexBoardElement.appendChild(this._selectedFlexElement) // Move element to the first position
 
       if (this._transform) {
         this._offset.x -= this._transform.e
         this._offset.y -= this._transform.f
       }
+    }
+
+    if (!draggableGroupElement) {
+      this._boardManager.onDragFlexComponent()
     }
   }
 }
