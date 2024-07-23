4c66cae644fd6fd6fb7efdbd645973e06bc7ccc2
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-22 23:37:44 -0300

    Create resize box

--- src/components/Board/subcomponents/ResizeBox.tsx
+++ src/components/Board/subcomponents/ResizeBox.tsx
@@ -5,6 +5,18 @@ export type ResizeBoxProps = {
   boardState: BoardState
 }
 
+type ResizerProps = {
+  cx: number
+  cy: number
+  rx: number
+  ry: number
+  id: string
+}
+
+const resizerClass = 'resizer'
+const rx = 5
+const ry = 5
+
 export function ResizeBox (props: ResizeBoxProps) {
   const { boardState } = props
 
@@ -16,28 +28,50 @@ export function ResizeBox (props: ResizeBoxProps) {
 
   const { properties: { x, y , width, height } } = selectedFlexComponent
 
+  const handleResizerClick = () => {
+
+  }
+
+  {/* https://en.wikipedia.org/wiki/Wind_rose */}
+  const resizers: ResizerProps[] = [
+    { cx: x + width / 2, cy: y, rx, ry, id: 'resize-n' },
+    { cx: x + width, cy: y, rx, ry, id: 'resize-ne' },
+    { cx: x + width, cy: y + height / 2, rx, ry, id: 'resize-e' },
+    { cx: x + width, cy: y + height, rx, ry, id: 'resize-se' },
+    { cx: x + width / 2, cy: y + height, rx, ry, id: 'resize-s' },
+    { cx: x, cy: y + height, rx, ry, id: 'resize-sw' },
+    { cx: x, cy: y + height / 2, rx, ry, id: 'resize-w'},
+    { cx: x, cy: y, rx, ry, id: 'resize-nw' },
+  ]
+
   return (
-    <g>
+    <g className={resizerClass}>
       <rect
         x={x}
         y={y}
         width={width}
         height={height}
+        className={resizerClass}
         fill="none"
         stroke="#00a8ff"
         strokeDasharray="3 3"
         pointerEvents="none"
         strokeWidth="2"
       />
-      {/* https://en.wikipedia.org/wiki/Wind_rose */}
-      <ellipse cx={x + width / 2} cy={y} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-n" />
-      <ellipse cx={x + width} cy={y} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-ne" />
-      <ellipse cx={x + width} cy={y + height / 2} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-e" />
-      <ellipse cx={x + width} cy={y + height} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-se" />
-      <ellipse cx={x + width / 2} cy={y + height} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-s" />
-      <ellipse cx={x} cy={y + height} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-sw" />
-      <ellipse cx={x} cy={y + height / 2} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-w" />
-      <ellipse cx={x} cy={y} rx="4" ry="4" fill="#0ea5e9" stroke="white" id="resize-nw" />
+      {resizers.map(resizer => (
+        <ellipse
+          key={resizer.id}
+          id={resizer.id}
+          className={resizerClass}
+          onClick={handleResizerClick}
+          cx={resizer.cx}
+          cy={resizer.cy}
+          rx={resizer.rx}
+          ry={resizer.ry}
+          fill="#0ea5e9"
+          stroke="white"
+        />
+      ))}
     </g>
   )
 }
