0338a4f75e62548a432866295133b6fd9cd9b916
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-23 21:58:19 -0300

    Change component drawer name

---
+++ src/components/ComponentsMenu/ComponentsMenu.tsx
@@ -0,0 +1,33 @@
+import { createElement, PropsWithChildren } from 'react'
+import { FLEX_COMPONENT_ICONS, FLEX_COMPONENT_NAMES } from '../../flex-components'
+import { FlexComponentType } from '../../types'
+import { BoardController } from '../../lib'
+import { Floating } from '../Floating'
+
+export type ComponentsMenuProps = PropsWithChildren & {
+  boardController: BoardController
+}
+
+export function ComponentsMenu (props: ComponentsMenuProps) {
+  const { boardController } = props
+
+  return (
+    <Floating className="top-20 left-4">
+      <ul className="menu bg-base-200 text-base-content min-h-full w-56 rounded-box h-[calc(100vh-6rem)]">
+        <li className="menu-title select-none">Components</li>
+        {Object.entries(FLEX_COMPONENT_NAMES).map(component => {
+          const type = component[0] as FlexComponentType
+          const name = component[1]
+
+          return (
+            <li key={component[0]}>
+              <a onClick={() => boardController.onAddFlexComponent(type)}>
+                {createElement(FLEX_COMPONENT_ICONS[type])}
+                {name}
+              </a>
+            </li>
+          )})}
+      </ul>
+    </Floating>
+  )
+}
