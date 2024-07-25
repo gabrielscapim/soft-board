e58b7c6babd58dee6cef8ada79a24d68ab4cab36
Gabriel Scapim <scapimgabriel@gmail.com>
2024-07-24 23:46:59 -0300

    Create base property input

--- src/components/FlexComponentPropertiesMenu/FlexComponentPropertiesMenu.tsx
+++ src/components/FlexComponentPropertiesMenu/FlexComponentPropertiesMenu.tsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from 'react'
 import { useSelectedFlexComponent } from '../../hooks'
 import { BoardController, BoardState } from '../../lib'
 import { Floating } from '../Floating'
-import { FlexComponentProperties } from '../../types'
+import { FlexComponentProperties, FlexComponentProperty } from '../../types'
+import { BasePropertyInput } from './subcomponents'
+
+
+const baseInputs: { name: string, property: FlexComponentProperty }[] = [
+  { name: 'X', property: 'x' },
+  { name: 'Y', property: 'y' },
+  { name: 'W', property: 'width' },
+  { name: 'H', property: 'height' }
+]
 
 export type FlexComponentPropertiesMenuProps = {
   boardState: BoardState
@@ -20,8 +29,8 @@ export function FlexComponentPropertiesMenu (props: FlexComponentPropertiesMenuP
     height: selectedFlexComponent?.properties.height ?? 0,
   })
 
-  const onUpdateProperties = (key: string, value: number) => {
-    setProperties(prevState => ({ ...prevState, [key]: value }))
+  const onUpdateProperties = (property: FlexComponentProperty, value: number) => {
+    setProperties(prevState => ({ ...prevState, [property]: value }))
   }
 
   const onBlur = () => {
@@ -39,7 +48,6 @@ export function FlexComponentPropertiesMenu (props: FlexComponentPropertiesMenuP
     }
   }, [selectedFlexComponent])
 
-
   return (
     <>
       {selectedFlexComponent && (
@@ -47,71 +55,16 @@ export function FlexComponentPropertiesMenu (props: FlexComponentPropertiesMenuP
           <ul className="menu bg-base-200 text-base-content min-h-full w-52 rounded-box h-[calc(100vh-6rem)]">
             <li className="menu-title select-none">{selectedFlexComponent.name}</li>
             <div className="px-2 my-2 flex flex-col gap-2">
-              <label className="input input-xs input-bordered flex items-center gap-2">
-                <span className="w-4">X</span>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="X-position"
-                  value={properties.x}
-                  onChange={event => onUpdateProperties('x', Number(event.target.value))}
+              {baseInputs.map(input => (
+                <BasePropertyInput
+                  key={`input-${input.property}`}
+                  property={input.property}
+                  name={input.name}
+                  properties={properties}
                   onBlur={onBlur}
-                  onKeyUp={event => {
-                    if (event.key === 'Enter') {
-                      onBlur()
-                    }
-                  }}
+                  onUpdateProperties={onUpdateProperties}
                 />
-              </label>
-              <label className="input input-xs input-bordered flex items-center gap-2">
-                <span className="w-4">Y</span>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="Y-position"
-                  value={properties.y}
-                  onChange={event => onUpdateProperties('y', Number(event.target.value))}
-                  onBlur={onBlur}
-                  onKeyUp={event => {
-                    if (event.key === 'Enter') {
-                      onBlur()
-                    }
-                  }}
-                />
-              </label>
-              <label className="input input-xs input-bordered flex items-center gap-2">
-                <span className="w-4">W</span>
-                <input
-                  type="text"
-                  className="grow"
-                  placeholder="Width"
-                  value={properties.width}
-                  onChange={event => onUpdateProperties('width', Number(event.target.value))}
-                  onBlur={onBlur}
-                  onKeyUp={event => {
-                    if (event.key === 'Enter') {
-                      onBlur()
-                    }
-                  }}
-                />
-              </label>
-              <label className="input input-xs input-bordered flex items-center gap-2">
-                <span className="w-4">H</span>
-                <input
-                  type="number"
-                  className="grow"
-                  placeholder="Height"
-                  value={properties.height}
-                  onChange={event => onUpdateProperties('height', Number(event.target.value))}
-                  onBlur={onBlur}
-                  onKeyDown={event => {
-                    console.log(event)
-                    if (event.key === 'Enter') {
-                      onBlur()
-                    }
-                  }}
-                />
-              </label>
+              ))}
             </div>
           </ul>
         </Floating>
---
+++ src/components/FlexComponentPropertiesMenu/subcomponents/BasePropertyInput.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react'
+import { FlexComponentProperties, FlexComponentProperty } from '../../../types'
+
+export type BasePropertyInputProps<T extends FlexComponentProperty> = {
+  property: T
+  name?: string
+  properties?: FlexComponentProperties
+  onBlur?: () => void
+  onUpdateProperties?: (property: T, value: number) => void
+}
+
+export function BasePropertyInput (props: BasePropertyInputProps<FlexComponentProperty>) {
+  const { property, name, properties, onUpdateProperties, onBlur } = props
+
+  const ref = useRef<HTMLInputElement>(null)
+
+  return (
+    <label className="input input-xs input-bordered flex items-center gap-2">
+      <span className="w-4">{name}</span>
+      <input
+        type="number"
+        className="grow"
+        ref={ref}
+        value={properties?.[property]}
+        onChange={event => onUpdateProperties?.(property, Number(event.target.value))}
+        onBlur={onBlur}
+        onKeyUp={event => {
+          if (event.key === 'Enter') {
+            ref.current!.blur()
+          }
+        }}
+        />
+    </label>
+  )
+}
