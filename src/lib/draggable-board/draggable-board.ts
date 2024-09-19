a32c383c2e9f0dde19e290636e8b8b65356ea7b7
Gabriel Scapim <scapimgabriel@gmail.com>
2024-09-19 07:36:11 -0300

    Refactor draggable board

--- src/lib/draggable-board/draggable-board.ts
+++ src/lib/draggable-board/draggable-board.ts
@@ -4,23 +4,22 @@ import { BoardManager } from '../board-manager'
 import { BoardState } from '../board-state'
 
 /**
- * Class responsible for leaving SVG components inside the board draggable.
+ * Class responsible to turn components inside the board draggable.
  */
 export class DraggableBoard {
   private _boardManager: BoardManager
   private _boardState: BoardState
-  private _flexBoardElement: SVGSVGElement
+  private _boardElement: HTMLElement
   private _offset: Offset | undefined
-  private _selectedFlexElement: SVGGElement | undefined
-  private _transform: DOMMatrix | undefined
+  private _selectedElement: HTMLDivElement | undefined
 
   constructor (
     boardState: BoardState,
-    flexBoardElement: SVGSVGElement,
+    boardElement: HTMLElement, // Adaptado para divs
   ) {
     this._boardManager = new BoardManager(boardState)
     this._boardState = boardState
-    this._flexBoardElement = flexBoardElement
+    this._boardElement = boardElement
 
     this.startDrag = this.startDrag.bind(this)
     this.onDragging = this.onDragging.bind(this)
@@ -28,19 +27,20 @@ export class DraggableBoard {
   }
 
   private getMousePosition (event: MouseEvent) {
-    const screenCTM = this._flexBoardElement.getScreenCTM()
-    const x = screenCTM ? (event.clientX - screenCTM.e) / screenCTM.a : 0
-    const y = screenCTM ? (event.clientY - screenCTM.f) / screenCTM.d : 0
+    const rect = this._boardElement.getBoundingClientRect()
+
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
 
     return { x, y }
   }
 
   public endDrag () {
-    this._selectedFlexElement = undefined
+    this._selectedElement = undefined
   }
 
   public onDragging (event: MouseEvent) {
-    if (this._selectedFlexElement && this._offset) {
+    if (this._selectedElement && this._offset) {
       event.preventDefault()
 
       const grid = this._boardState.grid
@@ -51,7 +51,7 @@ export class DraggableBoard {
       const roundedDeltaY = Math.round(deltaY / grid) * grid
 
       this._boardManager.onDraggingFlexComponent({
-        id: this._selectedFlexElement.id as UUID,
+        id: this._selectedElement.id as UUID,
         properties: {
           roundedDeltaX,
           roundedDeltaY
@@ -61,21 +61,14 @@ export class DraggableBoard {
   }
 
   public startDrag (event: MouseEvent) {
-    const target = event.target as SVGGElement
-    const draggableGroupElement = target.closest('.draggable-group') as SVGGElement | null
-    const resizerElement = target.closest('.resizer') as SVGEllipseElement | null
+    const target = event.target as HTMLDivElement
+    const draggableGroupElement = target.closest('.draggable-group') as HTMLDivElement | null
+    const resizerElement = target.closest('.resizer') as HTMLDivElement | null
 
     if (draggableGroupElement) {
-      this._selectedFlexElement = draggableGroupElement
+      this._selectedElement = draggableGroupElement
       this._offset = this.getMousePosition(event)
-      this._transform = draggableGroupElement.transform.baseVal.consolidate()?.matrix
       this._boardManager.onStartDragFlexComponent({ id: draggableGroupElement.id as UUID })
-      // this._flexBoardElement.appendChild(this._selectedFlexElement) // Move element to the first position
-
-      if (this._transform) {
-        this._offset.x -= this._transform.e
-        this._offset.y -= this._transform.f
-      }
     }
 
     if (!draggableGroupElement && !resizerElement) {
