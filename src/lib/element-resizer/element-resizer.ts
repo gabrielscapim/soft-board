ccd701e4467c7d06fdecfb8457333d2935174716
Gabriel Scapim <scapimgabriel@gmail.com>
2024-09-22 18:54:35 -0300

    Update element resizer to use html elements

--- src/lib/element-resizer/element-resizer.ts
+++ src/lib/element-resizer/element-resizer.ts
@@ -6,18 +6,18 @@ export class ElementResizer {
   private _boardManager: BoardManager
   private _boardState: BoardState
   private _resizeDirection: ResizeDirection | undefined
-  private _flexBoardElement: SVGSVGElement
+  private _boardElement: HTMLElement
   private _offset: Offset | undefined
-  private _selectedResizerElement: SVGGElement | undefined
+  private _selectedResizerElement: HTMLElement | undefined
   private _transform: DOMMatrix | undefined
 
   constructor (
     boardState: BoardState,
-    flexBoardElement: SVGSVGElement,
+    boardElement: HTMLElement
   ) {
     this._boardManager = new BoardManager(boardState)
     this._boardState = boardState
-    this._flexBoardElement = flexBoardElement
+    this._boardElement = boardElement
 
     this.startResize = this.startResize.bind(this)
     this.onResizing = this.onResizing.bind(this)
@@ -25,9 +25,10 @@ export class ElementResizer {
   }
 
   private getMousePosition (event: MouseEvent) {
-    const screenCTM = this._flexBoardElement.getScreenCTM()
-    const x = screenCTM ? (event.clientX - screenCTM.e) / screenCTM.a : 0
-    const y = screenCTM ? (event.clientY - screenCTM.f) / screenCTM.d : 0
+    const rect = this._boardElement.getBoundingClientRect()
+
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
 
     return { x, y }
   }
@@ -37,77 +38,79 @@ export class ElementResizer {
   }
 
   public onResizing (event: MouseEvent) {
-    if (this._selectedResizerElement && this._offset) {
-      event.preventDefault()
+    if (!this._selectedResizerElement || !this._offset) return
 
-      const grid = this._boardState.grid
-      const coord = this.getMousePosition(event)
-      const params = {
-        dimension: {
-          roundedDeltaX: 0,
-          roundedDeltaY: 0
-        },
-        position: {
-          roundedDeltaX: 0,
-          roundedDeltaY: 0
-        }
+    event.preventDefault()
+
+    const grid = this._boardState.grid
+    const coord = this.getMousePosition(event)
+    const params = {
+      dimension: {
+        roundedDeltaX: 0,
+        roundedDeltaY: 0
+      },
+      position: {
+        roundedDeltaX: 0,
+        roundedDeltaY: 0
       }
-      const deltaX = coord.x - (this._offset.x ?? 0)
-      const deltaY = coord.y - (this._offset.y ?? 0)
-      const roundedDeltaX = Math.round(deltaX / grid) * grid
-      const roundedDeltaY = Math.round(deltaY / grid) * grid
-
-      switch (this._resizeDirection) {
-        case 'n':
-          params.dimension.roundedDeltaY = -roundedDeltaY
-          params.position.roundedDeltaY = roundedDeltaY
-          break
-        case 'ne':
-          params.dimension.roundedDeltaY = -roundedDeltaY
-          params.dimension.roundedDeltaX = roundedDeltaX
-          params.position.roundedDeltaY = roundedDeltaY
-          break
-        case 'e':
-          params.dimension.roundedDeltaX = roundedDeltaX
-          break
-        case 'se':
-          params.dimension.roundedDeltaX = roundedDeltaX
-          params.dimension.roundedDeltaY = roundedDeltaY
-          break
-        case 's':
-          params.dimension.roundedDeltaY = roundedDeltaY
-          break
-        case 'sw':
-          params.dimension.roundedDeltaY = roundedDeltaY
-          params.dimension.roundedDeltaX = -roundedDeltaX
-          params.position.roundedDeltaX = roundedDeltaX
-          break
-        case 'w':
-          params.dimension.roundedDeltaX = -roundedDeltaX
-          params.position.roundedDeltaX = roundedDeltaX
-          break
-        case 'nw':
-          params.dimension.roundedDeltaY = -roundedDeltaY
-          params.dimension.roundedDeltaX = -roundedDeltaX
-          params.position.roundedDeltaX = roundedDeltaX
-          params.position.roundedDeltaY = roundedDeltaY
-          break
-      }
-
-      this._boardManager.onResizingFlexComponent(params)
     }
+    const deltaX = (coord.x - (this._offset.x ?? 0)) / this._boardState.scale
+    const deltaY = (coord.y - (this._offset.y ?? 0)) / this._boardState.scale
+    const roundedDeltaX = Math.round(deltaX / grid) * grid
+    const roundedDeltaY = Math.round(deltaY / grid) * grid
+
+    switch (this._resizeDirection) {
+      case 'n': // movimenta
+        params.dimension.roundedDeltaY = -roundedDeltaY
+        params.position.roundedDeltaY = roundedDeltaY
+        break
+      case 'ne': // movimenta
+        params.dimension.roundedDeltaY = -roundedDeltaY
+        params.dimension.roundedDeltaX = roundedDeltaX
+        params.position.roundedDeltaY = roundedDeltaY
+        break
+      case 'e':
+        params.dimension.roundedDeltaX = roundedDeltaX
+        break
+      case 'se':
+        params.dimension.roundedDeltaX = roundedDeltaX
+        params.dimension.roundedDeltaY = roundedDeltaY
+        break
+      case 's':
+        params.dimension.roundedDeltaY = roundedDeltaY
+        break
+      case 'sw': // movimenta
+        params.dimension.roundedDeltaY = roundedDeltaY
+        params.dimension.roundedDeltaX = -roundedDeltaX
+        params.position.roundedDeltaX = roundedDeltaX
+        break
+      case 'w': // movimenta
+        params.dimension.roundedDeltaX = -roundedDeltaX
+        params.position.roundedDeltaX = roundedDeltaX
+        break
+      case 'nw': // movimenta
+        params.dimension.roundedDeltaY = -roundedDeltaY
+        params.dimension.roundedDeltaX = -roundedDeltaX
+        params.position.roundedDeltaX = roundedDeltaX
+        params.position.roundedDeltaY = roundedDeltaY
+        break
+    }
+
+    this._boardManager.onResizingFlexComponent(params)
   }
 
   public startResize (event: MouseEvent) {
-    const target = event.target as SVGGElement
-    const resizerElement = target.closest('.resizer') as SVGEllipseElement | null
+    const target = event.target as HTMLElement
+    const resizerElement = target.closest('.resizer') as HTMLElement | null
 
     if (resizerElement) {
       this._selectedResizerElement = resizerElement
       this._offset = this.getMousePosition(event)
-      this._transform = resizerElement.transform.baseVal.consolidate()?.matrix
       this._boardManager.onStartResizeFlexComponent()
       this._resizeDirection = resizerElement.id as ResizeDirection
+
+      const transformStyle = window.getComputedStyle(resizerElement).transform
+      this._transform = transformStyle !== 'none' ? new DOMMatrix(transformStyle) : undefined
 
       if (this._transform) {
         this._offset.x -= this._transform.e
