03942958f0f2419105afa5e14f34ff6670c65b75
Gabriel Scapim <scapimgabriel@gmail.com>
2024-09-22 11:04:39 -0300

    Add component

--- src/flex-components/flex-component-names.ts
+++ src/flex-components/flex-component-names.ts
@@ -2,5 +2,6 @@ import { FlexComponentName, FlexComponentType } from '../types'
 
 export const FLEX_COMPONENT_NAMES: Record<FlexComponentType, FlexComponentName> = {
   button: 'Button',
+  input: 'Input',
   rectangle: 'Rectangle'
 }
