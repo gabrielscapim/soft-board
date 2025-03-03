366498420e9e84752975b39ec4e2e0626c740da9
Gabriel Scapim <scapimgabriel@gmail.com>
2025-03-03 11:28:21 -0300

    Create state

--- src/types/board-state/board-event.ts
+++ src/types/board-state/board-event.ts
@@ -3,6 +3,7 @@ export type BoardEvent =
   'gridChanged' |
   'guidesChanged' |
   'isBoardMovingChanged' |
+  'isDraggingChanged' |
   'scaleChanged' |
   'selectedFlexComponentChanged' |
   'translateChanged'
