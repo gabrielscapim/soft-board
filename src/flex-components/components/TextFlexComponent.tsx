779162f60c6d92692e5c47585dd1403c6a8a01da
Gabriel Scapim <scapimgabriel@gmail.com>
2025-03-03 10:52:01 -0300

    Create properties menu

---
+++ src/flex-components/components/TextFlexComponent.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx'
+import { TextFlexComponentProperties } from '../../types'
+import { FlexComponentProps } from '../flex-components'
+
+export function TextFlexComponent (props: FlexComponentProps) {
+  const { component, boardController } = props
+
+  const properties = component.properties as TextFlexComponentProperties
+
+  return (
+    <span
+      id={component.id}
+      contentEditable
+      suppressContentEditableWarning
+      className={clsx(
+        'absolute',
+        'draggable-group',
+        'outline-none'
+      )}
+      style={{
+        top: `${properties.y}px`,
+        left: `${properties.x}px`,
+        width: `${properties.width}px`,
+        height: `${properties.height}px`,
+        fontSize: `${properties.fontSize}px`,
+        fontWeight: properties.bold ? 'bold' : 'normal',
+        textAlign: properties.align,
+        fontStyle: properties.italic ? 'italic' : 'normal',
+        overflow: 'hidden',
+        whiteSpace: 'normal',
+        wordBreak: 'break-word'
+      }}
+      onBlur={event => {
+        boardController.onUpdateFlexComponent({flexComponent: {  ...component, properties: { ...properties, text: event.currentTarget.textContent } as TextFlexComponentProperties }})
+      }}
+    >
+      {properties.text}
+    </span>
+  )
+}
