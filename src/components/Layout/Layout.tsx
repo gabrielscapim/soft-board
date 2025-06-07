840b0302d20f324ee35909b901c60f17a61bbdb9
Gabriel Scapim <scapimgabriel@gmail.com>
2025-06-07 14:30:48 -0300

    Add board header

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,14 @@
 import { Outlet } from 'react-router'
 import { BoardSidebar } from '../BoardSidebar'
-import { SidebarInset, SidebarProvider, SidebarTrigger } from '../ui/sidebar'
-import { Separator } from '../ui/separator'
+import { SidebarInset, SidebarProvider } from '../ui/sidebar'
+import { BoardHeader } from '../BoardHeader'
 
 export function Layout () {
   return (
     <SidebarProvider>
       <BoardSidebar />
       <SidebarInset>
-        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-2">
-          <SidebarTrigger className="m-0" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-        </header>
+        <BoardHeader />
         <main>
           <Outlet />
         </main>
