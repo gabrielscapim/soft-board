7c49207b8f0e4ccbf50826b9abc8d7ed6bfec985
Gabriel Scapim <scapimgabriel@gmail.com>
2025-06-19 12:17:55 -0300

    Radio button propertie menu

---
+++ src/flex-components/components/RadioButton/PropertiesMenu/RadioButtonPropertiesMenu.tsx
@@ -0,0 +1,24 @@
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+import { BoardPropertiesMenuProps } from '@/routes/BoardRoute/components'
+import { RadioButtonFlexComponentProperties } from '@/types'
+
+export function RadioButtonPropertiesMenu (props: BoardPropertiesMenuProps) {
+  const { selected } = props
+
+  const properties = selected.properties as RadioButtonFlexComponentProperties
+
+  return (
+    <>
+      <Label className="flex flex-row items-center justify-between border-input p-3 border rounded-md">
+        <div className="flex flex-col gap-1">
+          Activated
+          <p className="text-xs text-muted-foreground">
+            State of the radio button.
+          </p>
+        </div>
+        <Switch id="shape-fill" checked={properties.activated} />
+      </Label>
+    </>
+  )
+}
