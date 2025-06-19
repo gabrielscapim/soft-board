b50557ee45af6223b65b774a6e85615087e6a0b6
Gabriel Scapim <scapimgabriel@gmail.com>
2025-06-19 14:56:17 -0300

    Refactor BoardPropertiesMenu to use Tabs for better organization of properties, layout, and actions

--- src/routes/BoardRoute/components/BoardPropertiesMenu/BoardPropertiesMenu.tsx
+++ src/routes/BoardRoute/components/BoardPropertiesMenu/BoardPropertiesMenu.tsx
@@ -1,10 +1,9 @@
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { FLEX_COMPONENT_NAMES } from '@/flex-components'
-import { FLEX_COMPONENTS_PROPERTIES_MENU } from '@/flex-components/registry/properties-menu'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { BoardController, BoardState } from '@/lib'
 import { FlexComponent } from '@/types'
 import clsx, { ClassValue } from 'clsx'
+import { ActionsTabContent, LayoutTabContent, PropertiesTabContent } from './Tabs'
+import { useState, useEffect } from 'react'
 
 export type BoardPropertiesMenuProps = {
   boardState: BoardState
@@ -13,64 +12,58 @@ export type BoardPropertiesMenuProps = {
   className?: ClassValue
 }
 
+export type BoardPropertiesMenuTabProps = {
+  boardState: BoardState
+  boardController: BoardController
+  selected: FlexComponent
+}
+
+const TABS = [
+  {
+    value: 'properties',
+    label: 'Properties',
+    content: PropertiesTabContent,
+  },
+  {
+    value: 'layout',
+    label: 'Layout',
+    content: LayoutTabContent,
+  },
+  {
+    value: 'actions',
+    label: 'Actions',
+    content: ActionsTabContent
+  }
+]
+
 export function BoardPropertiesMenu (props: BoardPropertiesMenuProps) {
   const { selected, className } = props
 
-  const Menu = FLEX_COMPONENTS_PROPERTIES_MENU[selected.type]
+  const [tab, setTab] = useState('properties')
+
+  useEffect(() => {
+    setTab('properties')
+  }, [selected.id])
 
   return (
     <div
       className={clsx('bg-sidebar absolute top-20 right-4 rounded-xl border w-80 shadow-md', className)}
       style={{ zIndex: 100_000 }}
     >
-      <div className="gap-3.5 border-b p-4">
-        <div className="text-foreground text-base font-medium">
-          {FLEX_COMPONENT_NAMES[selected.type]} properties
+      <Tabs value={tab} className="gap-0" onValueChange={setTab}>
+        <div className="mx-3 mb-0 mt-3">
+          <TabsList className="w-full">
+            <TabsTrigger value="properties">Properties</TabsTrigger>
+            <TabsTrigger value="layout">Layout</TabsTrigger>
+            <TabsTrigger value="actions">Actions</TabsTrigger>
+          </TabsList>
         </div>
-      </div>
-      <div className="p-4 flex flex-col gap-4">
-        <Label className="flex flex-col items-start">
-          Name
-          <Input value={selected.name} />
-          <span className="text-xs text-muted-foreground">
-            Used to identify the component on the board.
-          </span>
-        </Label>
-
-        <div>
-          <p className="text-sm leading-none font-medium select-none pb-3">
-            Dimension
-          </p>
-          <div className="grid grid-cols-2 gap-2">
-            <Label className="text-xs">
-              W
-              <Input type="number" value={selected.properties.width} />
-            </Label>
-            <Label className="text-xs">
-              H
-              <Input type="number" value={selected.properties.height} />
-            </Label>
-          </div>
-        </div>
-
-        <div>
-          <p className="text-sm leading-none font-medium select-none pb-3">
-            Position
-          </p>
-          <div className="grid grid-cols-2 gap-2">
-            <Label className="text-xs">
-              X
-              <Input type="number" value={selected.properties.x} />
-            </Label>
-            <Label className="text-xs">
-              Y
-              <Input type="number" value={selected.properties.y} />
-            </Label>
-          </div>
-        </div>
-
-        {Menu && <Menu {...props} />}
-      </div>
+        {TABS.map(tab => (
+          <TabsContent key={tab.value} value={tab.value} className="p-4 flex flex-col gap-4">
+            <tab.content {...props} />
+          </TabsContent>
+        ))}
+      </Tabs>
     </div>
   )
 }
