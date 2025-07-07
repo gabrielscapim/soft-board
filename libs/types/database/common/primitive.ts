10ca0dddf25d3a2e9658dbe05874fcf35d6451d5
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-07 11:54:16 -0300

    Add primitive types for database schema

---
+++ libs/types/database/common/primitive.ts
@@ -0,0 +1,5 @@
+export type TEXT = string
+export type TIMESTAMPTZ = Date
+export type UUID = `${string}-${string}-${string}-${string}-${string}`
+export type VARCHAR_255 = string
+export type VARCHAR_50 = string
