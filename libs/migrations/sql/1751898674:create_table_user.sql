bb2df84ce57d95c8c2b1551d324a9fded6488130
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-07 11:41:41 -0300

    Make email and password_hash fields NOT NULL in user table

--- libs/migrations/sql/1751898674:create_table_user.sql
+++ libs/migrations/sql/1751898674:create_table_user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "user" (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
   normalized_email VARCHAR(255) UNIQUE,
-  password_hash TEXT,
+  password_hash TEXT NOT NULL,
   create_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   update_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
