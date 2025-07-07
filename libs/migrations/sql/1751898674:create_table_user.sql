389d35c46f74af45ef0b73cb9a3e03af0049b41a
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-07 11:50:32 -0300

    Make password_hash field nullable in user table

--- libs/migrations/sql/1751898674:create_table_user.sql
+++ libs/migrations/sql/1751898674:create_table_user.sql
@@ -3,7 +3,7 @@ CREATE TABLE "user" (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   normalized_email VARCHAR(255) UNIQUE,
-  password_hash TEXT NOT NULL,
+  password_hash TEXT,
   create_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   update_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
