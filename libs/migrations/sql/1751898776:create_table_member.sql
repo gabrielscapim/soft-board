ff3f20e53ac43c18cbea62fae7ec82ae15d83b89
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 19:50:00 -0300

    Refactor member table to use member_role enum for role column

--- libs/migrations/sql/1751898776:create_table_member.sql
+++ libs/migrations/sql/1751898776:create_table_member.sql
@@ -8,7 +8,7 @@ CREATE TABLE member (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID NOT NULL REFERENCES "user"(id) ON DELETE CASCADE,
   team_id UUID NOT NULL REFERENCES team(id) ON DELETE CASCADE,
-  role VARCHAR(50) NOT NULL,
+  role member_role NOT NULL DEFAULT 'member',
   create_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   update_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
