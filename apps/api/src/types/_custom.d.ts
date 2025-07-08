15a8f3e11869d25c0b37846b96c31064a4243833
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 11:27:27 -0300

    Add TeamData type and update type exports in custom types

--- apps/api/src/types/_custom.d.ts
+++ apps/api/src/types/_custom.d.ts
@@ -1,9 +1,11 @@
 import type { AuthenticationData } from './authentication-data'
+import { TeamData } from './team-data'
 
 declare global {
   namespace Express {
     interface Request {
       auth?: AuthenticationData
+      team?: TeamData
     }
   }
 }
--- apps/api/src/types/index.ts
+++ apps/api/src/types/index.ts
@@ -1,2 +1,3 @@
 export * from './authentication-data'
 export * from './endpoint'
+export * from './team-data'
