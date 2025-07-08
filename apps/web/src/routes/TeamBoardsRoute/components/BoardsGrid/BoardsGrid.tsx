7c5d96a5500ae9eee8469e48425e22d955d8a133
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 13:25:00 -0300

    Add BoardsGrid component for displaying boards with loading skeletons

---
+++ apps/web/src/routes/TeamBoardsRoute/components/BoardsGrid/BoardsGrid.tsx
@@ -0,0 +1,30 @@
+import { GetBoardsResultData } from 'types/endpoints'
+import { BoardCard } from '../BoardCard'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export type BoardsGridProps = {
+  loading?: boolean
+  boards?: GetBoardsResultData[]
+}
+
+export function BoardsGrid (props: BoardsGridProps) {
+  const { loading, boards = [] } = props
+
+  return (
+    <div className="grid gap-8 w-full lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-1">
+      {!loading && boards.map(board => (
+        <BoardCard key={board.id} board={board} />
+      ))}
+
+      {loading && new Array(9).fill(null).map((_, index) => (
+        <div key={index} className="w-full">
+          <Skeleton className="h-64 w-full" />
+          <div className="p-3">
+            <Skeleton className="h-6 w-3/4 mb-2" />
+            <Skeleton className="h-4 w-1/2" />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
