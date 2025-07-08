2f33b716a5498d70371229260493ef49af76f298
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 19:55:11 -0300

    Add tests for getMembers endpoint and create index file for exports

---
+++ apps/api/src/endpoints/getMembers/getMembers.test.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from 'vitest'
+import { DatabaseFactory, getPool } from '../../libs'
+import { createApp } from '../../setup'
+import * as getMembers from './getMembers'
+import request from 'supertest'
+
+describe('getMembers', () => {
+  test('return members', async () => {
+    const pool = getPool()
+    const factory = new DatabaseFactory({ pool })
+    const user1 = await factory.createUser()
+    const user2 = await factory.createUser()
+    const user3 = await factory.createUser()
+    const team = await factory.createTeam()
+    await factory.createMember({ userId: user1.id, teamId: team.id })
+    await factory.createMember({ userId: user2.id, teamId: team.id })
+    await factory.createMember({ userId: user3.id, teamId: team.id })
+
+    const app = createApp({
+      endpoints: { getMembers },
+      tests: {
+        auth: { userId: user1.id },
+        team: { teamId: team.id }
+      }
+    })
+
+    const response = await request(app).post('/getMembers')
+
+    expect(response.body.data.length).toBe(3)
+  })
+})
