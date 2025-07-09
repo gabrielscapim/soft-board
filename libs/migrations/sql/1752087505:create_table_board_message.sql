e97aa2ee8fb641587f7ac57237a3afa000adf686
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 16:04:55 -0300

    Create board_message table and board_message_role enum type

---
+++ libs/migrations/sql/1752087505:create_table_board_message.sql
@@ -0,0 +1,21 @@
+CREATE TYPE board_message_role AS ENUM (
+  'assistant',
+  'system',
+  'tool',
+  'user'
+);
+
+CREATE TABLE board_message (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  board_id UUID NOT NULL REFERENCES board(id) ON DELETE CASCADE,
+  author_id UUID REFERENCES "user"(id) ON DELETE SET NULL,
+  content TEXT,
+  role board_message_role NOT NULL,
+  tool_call_id TEXT,
+  tool_calls JSONB,
+  send_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  create_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  update_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON board_message (board_id);
