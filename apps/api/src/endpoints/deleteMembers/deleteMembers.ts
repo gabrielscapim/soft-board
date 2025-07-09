d3944f9826294d93d3c46643e2cf38b0c66c3804
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 22:27:32 -0300

    Refactor deleteMembers endpoint to include user validation and prevent self-deletion

--- apps/api/src/endpoints/deleteMembers/deleteMembers.ts
+++ apps/api/src/endpoints/deleteMembers/deleteMembers.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from 'express'
 import { DeleteMembersCommand } from 'types/endpoints'
 import * as yup from 'yup'
 import { getPool } from '../../libs'
+import { BadRequest } from 'http-errors'
 
 type Handler = RequestHandler<unknown, unknown, DeleteMembersCommand>
 
@@ -12,9 +13,21 @@ const schema = yup.object({
 export function handler (): Handler {
   return async (req, res) => {
     const teamId = req.team!.teamId
+    const userId = req.auth?.userId
     const { memberIds } = schema.validateSync(req.body, { abortEarly: false })
 
     const pool = getPool()
+
+    const clientMember = await pool
+      .SELECT`id`
+      .FROM`member`
+      .WHERE`team_id = ${teamId}`
+      .AND`user_id = ${userId}`
+      .find()
+
+    if (memberIds.includes(clientMember.id)) {
+      throw new BadRequest('You cannot delete yourself from the team')
+    }
 
     await pool
       .DELETE_FROM`member`
