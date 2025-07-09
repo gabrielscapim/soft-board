07e106f0fa844ca4192d0e48aa549be277428a1e
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 10:32:29 -0300

    Refactor leaveTeam tests to improve structure and clarity

--- apps/api/src/endpoints/leaveTeam/leaveTeam.test.ts
+++ apps/api/src/endpoints/leaveTeam/leaveTeam.test.ts
@@ -5,30 +5,54 @@ import { createApp } from '../../setup'
 import request from 'supertest'
 
 describe('leaveTeam', () => {
-  test('leave team', async () => {
-    const pool = getPool()
-    const factory = new DatabaseFactory({ pool })
-    const user = await factory.createUser()
-    const team = await factory.createTeam()
-    const member = await factory.createMember({ userId: user.id, teamId: team.id })
+  describe('when you are not the owner', () => {
+    test('leave team', async () => {
+      const pool = getPool()
+      const factory = new DatabaseFactory({ pool })
+      const user = await factory.createUser()
+      const team = await factory.createTeam()
+      const member = await factory.createMember({ userId: user.id, teamId: team.id, role: 'member' })
 
-    const app = createApp({
-      endpoints: { leaveTeam },
-      tests: {
-        auth: { userId: user.id },
-        team: { teamId: team.id, memberRole: member.role }
-      }
+      const app = createApp({
+        endpoints: { leaveTeam },
+        tests: {
+          auth: { userId: user.id },
+          team: { teamId: team.id, memberRole: member.role }
+        }
+      })
+
+      await request(app).post('/leaveTeam')
+
+      const check = await pool
+        .SELECT`id`
+        .FROM`member`
+        .WHERE`team_id = ${team.id}`
+        .AND`user_id = ${user.id}`
+        .first()
+
+      expect(check).toBeNull()
     })
+  })
 
-    await request(app).post('/leaveTeam')
+  describe('when you are the owner', () => {
+    test('cannot leave team', async () => {
+      const pool = getPool()
+      const factory = new DatabaseFactory({ pool })
+      const user = await factory.createUser()
+      const team = await factory.createTeam()
+      await factory.createMember({ userId: user.id, teamId: team.id, role: 'owner' })
 
-    const check = await pool
-      .SELECT`id`
-      .FROM`member`
-      .WHERE`team_id = ${team.id}`
-      .AND`user_id = ${user.id}`
-      .first()
+      const app = createApp({
+        endpoints: { leaveTeam },
+        tests: {
+          auth: { userId: user.id },
+          team: { teamId: team.id, memberRole: 'owner' }
+        }
+      })
 
-    expect(check).toBeNull()
+      const response = await request(app).post('/leaveTeam')
+
+      expect(response.status).toBe(400)
+    })
   })
 })
