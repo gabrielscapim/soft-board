34516d5eb9ca1543707b70f27bbb72de01218cf9
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-08 23:53:50 -0300

    Implement updateTeam endpoint and associated types

---
+++ apps/api/src/endpoints/updateTeam/updateTeam.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express'
+import { UpdateTeamCommand } from 'types/endpoints'
+import * as yup from 'yup'
+import { getPool } from '../../libs'
+
+type Handler = RequestHandler<unknown, unknown, UpdateTeamCommand>
+
+const schema = yup.object({
+  name: yup.string().trim().required('Name is required').max(100, 'Name must be at most 100 characters long')
+})
+
+export function handler (): Handler {
+  return async (req, res) => {
+    const teamId = req.team!.teamId
+    const { name } = schema.validateSync(req.body, { abortEarly: false })
+
+    const pool = getPool()
+
+    await pool
+      .UPDATE`team`
+      .SET({
+        name,
+        updateDate: new Date()
+      })
+      .WHERE`id = ${teamId}`
+
+    res.status(204).end()
+  }
+}
