79e832ce698e260eca649332ea7bae115c2eb42f
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 15:56:15 -0300

    Reduce padding in ChatBubbleMessage component from 4 to 3

--- apps/web/src/components/ui/chat/chat-bubble.tsx
+++ apps/web/src/components/ui/chat/chat-bubble.tsx
@@ -72,7 +72,7 @@ const ChatBubbleAvatar: React.FC<ChatBubbleAvatarProps> = ({
 )
 
 // ChatBubbleMessage
-const chatBubbleMessageVariants = cva('p-4', {
+const chatBubbleMessageVariants = cva('p-3', {
   variants: {
     variant: {
       received:
