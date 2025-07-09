a7c874f3cf199252c4b7b91d9f6dd34025a0c6cc
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 10:22:02 -0300

    Enhance leaveTeam endpoint to check member role before leaving the team

--- apps/api/src/endpoints/leaveTeam/leaveTeam.ts
+++ apps/api/src/endpoints/leaveTeam/leaveTeam.ts
@@ -1,5 +1,9 @@
 import { RequestHandler } from 'express'
 import { getPool } from '../../libs'
+import { MemberDatabase } from 'types/database'
+import { BadRequest } from 'http-errors'
+
+type MemberRow = Pick<MemberDatabase, 'id' | 'role'>
 
 export function handler (): RequestHandler {
   return async (req, res) => {
@@ -9,11 +13,15 @@ export function handler (): RequestHandler {
     const pool = getPool()
 
     const member = await pool
-      .SELECT`id`
+      .SELECT<MemberRow>`id, role`
       .FROM`member`
       .WHERE`team_id = ${teamId}`
       .AND`user_id = ${userId}`
       .find({ error: 'You are not a member of this team' })
+
+    if (member.role === 'owner') {
+      throw new BadRequest('You cannot leave a team if you are the owner.')
+    }
 
     await pool
       .DELETE_FROM`member`
