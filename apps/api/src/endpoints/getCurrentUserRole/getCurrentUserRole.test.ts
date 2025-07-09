d56df16fa8e8bc8e83858a9abbc052157f8f8047
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 09:48:15 -0300

    Add tests for getCurrentUserRole endpoint

---
+++ apps/api/src/endpoints/getCurrentUserRole/getCurrentUserRole.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from 'vitest'
+import { createApp } from '../../setup'
+import * as getCurrentUserRole from './getCurrentUserRole'
+import request from 'supertest'
+import { DatabaseFactory, getPool } from '../../libs'
+
+describe('getCurrentUserRole', () => {
+  test('return the current user role', async () => {
+    const pool = getPool()
+    const factory = new DatabaseFactory({ pool })
+    const user = await factory.createUser()
+    const team = await factory.createTeam()
+    await factory.createMember({ userId: user.id, teamId: team.id, role: 'admin' })
+
+    const app = createApp({
+      endpoints: { getCurrentUserRole },
+      tests: {
+        auth: { userId: user.id },
+        team: { teamId: team.id, memberRole: 'owner' }
+      }
+    })
+
+    const response = await request(app)
+      .post('/getCurrentUserRole')
+      .expect(200)
+
+    expect(response.body.role).toBe('admin')
+  })
+})
