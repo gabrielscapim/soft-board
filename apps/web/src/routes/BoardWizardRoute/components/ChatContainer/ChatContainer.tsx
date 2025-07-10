108d262b37cfb1508cd6ae697bb1aa45dbf67708
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-09 23:50:29 -0300

    Add ChatContainer component and related exports for chat functionality

---
+++ apps/web/src/routes/BoardWizardRoute/components/ChatContainer/ChatContainer.tsx
@@ -0,0 +1,33 @@
+import { GetAuthenticatedUserResult, GetBoardResult, GetMessagesResultData } from 'types/endpoints'
+import { Card } from '@/components/ui/card'
+import { ChatContainerHeader } from './ChatContainerHeader'
+import { ChatMessagesList } from './ChatContainerMessagesList'
+import { ChatContainerInput } from './ChatContainerInput'
+
+export type ChatContainerProps = {
+  board: GetBoardResult
+  authenticatedUser?: GetAuthenticatedUserResult
+  loading?: boolean
+  messages?: GetMessagesResultData[]
+  onSendMessage?: (content: string) => void
+}
+
+export function ChatContainer (props: ChatContainerProps) {
+  const { board, authenticatedUser, loading, messages, onSendMessage } = props
+
+  return (
+    <Card className="h-full w-2/6 text-sm gap-0 py-0 shadow-none">
+      <ChatContainerHeader board={board} />
+      <ChatMessagesList
+        board={board}
+        loading={loading}
+        authenticatedUser={authenticatedUser}
+        messages={messages}
+      />
+      <ChatContainerInput
+        loading={loading}
+        onSendMessage={onSendMessage}
+      />
+    </Card>
+  )
+}
