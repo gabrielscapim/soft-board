1bbfb05abe4695f8ddc847504e9852f16d794efd
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-11 08:29:39 -0300

    Add requirement table with necessary fields and index for board_id

---
+++ libs/migrations/sql/1752232996:create_table_requirement.sql
@@ -0,0 +1,12 @@
+CREATE TABLE requirement (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  team_id UUID NOT NULL REFERENCES team(id) ON DELETE CASCADE,
+  board_id UUID NOT NULL REFERENCES board(id) ON DELETE CASCADE,
+  author_id UUID REFERENCES "user"(id) ON DELETE SET NULL,
+  content TEXT,
+  "order" INTEGER NOT NULL DEFAULT 0,
+  create_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  update_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX ON requirement (board_id);
