0ccbe711c4bd22e40b39276ca620746bdf67c40b
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-11 14:30:08 -0300

    Add tests for deleteRequirement endpoint

---
+++ apps/api/src/endpoints/deleteRequirement/deleteRequirement.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, test } from 'vitest'
+import request from 'supertest'
+import { getPool, DatabaseFactory } from '../../libs'
+import { createApp } from '../../setup'
+import * as deleteRequirement from './deleteRequirement'
+
+describe('deleteRequirement', () => {
+  test('delete a board requirement', async () => {
+    const pool = getPool()
+    const factory = new DatabaseFactory({ pool })
+    const user = await factory.createUser()
+    const team = await factory.createTeam()
+    await factory.createMember({ userId: user.id, teamId: team.id })
+    const board = await factory.createBoard({ teamId: team.id, authorId: user.id })
+    const requirement = await factory.createRequirement({ boardId: board.id, teamId: team.id, authorId: user.id })
+
+    const app = createApp({
+      endpoints: { deleteRequirement },
+      tests: {
+        auth: { userId: user.id },
+        team: { teamId: team.id, memberRole: 'owner' }
+      }
+    })
+
+    await request(app)
+      .post('/deleteRequirement')
+      .send({ id: requirement.id, boardId: board.id })
+
+    const check = await pool
+      .SELECT`id`
+      .FROM`requirement`
+      .WHERE`id = ${requirement.id}`
+      .AND`board_id = ${board.id}`
+      .AND`team_id = ${team.id}`
+      .first()
+
+    expect(check).toBeNull()
+  })
+})
