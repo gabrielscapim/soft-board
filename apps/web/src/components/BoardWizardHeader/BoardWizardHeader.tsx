57422d0e5a0ed3ef0255575f709efbd10c244250
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-14 17:45:31 -0300

    Refactor BoardWizardHeader component to simplify layout and remove unused imports

--- apps/web/src/components/BoardWizardHeader/BoardWizardHeader.tsx
+++ apps/web/src/components/BoardWizardHeader/BoardWizardHeader.tsx
@@ -1,44 +1,24 @@
 import { useSelectedBoard } from '@/hooks'
-import { Avatar, AvatarImage } from '../ui/avatar'
 import { Link, useParams } from 'react-router'
-import { getRootImage } from '@/helpers'
 import { Button } from '../ui/button'
 import { ChevronLeft } from 'lucide-react'
-import { Tooltip, TooltipContent } from '../ui/tooltip'
-import { TooltipTrigger } from '@radix-ui/react-tooltip'
-import { Skeleton } from '../ui/skeleton'
-import { Separator } from '../ui/separator'
 
 export function BoardWizardHeader () {
   const params = useParams<{ boardId?: string }>()
   const boardId = params.boardId
-  const { board, loading } = useSelectedBoard(boardId)
+  const { board } = useSelectedBoard(boardId)
 
   return (
-    <header className="flex flex-row items-center bg-background sticky top-0 shrink-0 border-b p-2 z-50 gap-3">
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link to={`/${board?.team.slug ?? ''}/boards`} className="flex items-center">
-            <Button variant="outline" size="icon" className="size-7">
-              <ChevronLeft />
-            </Button>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">
-          <p>Back to boards</p>
-        </TooltipContent>
-      </Tooltip>
-      <Separator
-        orientation="vertical"
-        className="mx-1 data-[orientation=vertical]:h-4"
-      />
-      <div className="text-sm flex flex-row items-center gap-2">
-        <Avatar>
-          <AvatarImage src={getRootImage(board?.image)} />
-        </Avatar>
-        {loading && <Skeleton className="w-32 h-4 " />}
-        {!loading && (board?.title ?? <span className="opacity-30">Untitled Board</span>)}
-      </div>
+    <header className="flex flex-row justify-between items-center sticky top-0 shrink-0 p-2 z-50 bg-muted">
+      <Link
+        to={`/${board?.team.slug ?? ''}/boards`}
+        className="flex items-center gap-2 font-medium"
+      >
+        <Button variant="outline">
+          <ChevronLeft />
+          Back
+        </Button>
+      </Link>
     </header>
   )
 }
