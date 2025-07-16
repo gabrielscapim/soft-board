d142ef58fddd18b6c0a4304f9afcd13535998bd6
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-16 15:02:16 -0300

    Refactor App and BoardContextProvider components to simplify state management and improve routing structure

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,74 +2,97 @@ import { Navigate, Route, Routes } from 'react-router'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BoardContextProvider } from './contexts/BoardContext/BoardContextProvider'
-import { BoardController, BoardState } from './lib'
-import { useState } from 'react'
-import { AuthenticationGuardLayout, BoardLayout, BoardWizardLayout, RootLayout, UnauthenticatedGuardLayout } from './components'
-import { ErrorRoute, SignInRoute, BoardsRoute, MembersRoute, SettingsRoute, BoardWizardRoute, BoardRoute } from './routes'
+import {
+  AuthenticationGuardLayout,
+  BoardLayout,
+  BoardWizardLayout,
+  RootLayout,
+  UnauthenticatedGuardLayout
+} from './components'
+import {
+  ErrorRoute,
+  SignInRoute,
+  BoardsRoute,
+  MembersRoute,
+  SettingsRoute,
+  BoardWizardRoute,
+  BoardRoute
+} from './routes'
 import { Toaster } from 'sonner'
-import { AuthenticationProvider, AuthorizationProvider, ClientProvider, TeamProvider } from './contexts'
+import {
+  AuthenticationProvider,
+  AuthorizationProvider,
+  ClientProvider,
+  TeamProvider
+} from './contexts'
 import { Client } from './client'
 
 const client = new Client()
 const queryClient = new QueryClient()
 
 function App () {
-  const [boardState] = useState(new BoardState())
-  const boardController = new BoardController(boardState)
-
   return (
     <QueryClientProvider client={queryClient}>
       <ClientProvider client={client}>
         <AuthenticationProvider>
-          <BoardContextProvider boardState={boardState} boardController={boardController}>
-            <Toaster />
-            <div className="bg-muted">
-              <Routes>
-                <Route path="/board-test" element={<BoardLayout />}>
-                  <Route index element={<BoardRoute />} />
+          <Toaster />
+          <div className="bg-muted">
+            <Routes>
+
+              {/* Public Routes */}
+              <Route path="/" element={<UnauthenticatedGuardLayout />}>
+                <Route index element={<SignInRoute />} />
+                <Route path="sign-in" element={<SignInRoute />} />
+              </Route>
+
+              <Route path="/board-test" element={<BoardLayout />}>
+                <Route index element={<BoardRoute />} />
+              </Route>
+
+              {/* Private Routes */}
+              <Route path=":teamSlug" element={<AuthenticationGuardLayout />}>
+                <Route
+                  element={
+                    <TeamProvider>
+                      <AuthorizationProvider>
+                        <RootLayout />
+                      </AuthorizationProvider>
+                    </TeamProvider>
+                  }
+                >
+                  <Route index element={<Navigate to="boards" replace />} />
+                  <Route path="boards" element={<BoardsRoute />} />
+                  <Route path="members" element={<MembersRoute />} />
+                  <Route path="settings" element={<SettingsRoute />} />
                 </Route>
 
-                {/* Public Routes */}
-                <Route path="/" element={<UnauthenticatedGuardLayout />}>
-                  <Route index element={<SignInRoute />} />
-                  <Route path="sign-in" element={<SignInRoute />} />
-                </Route>
-
-                {/* Private Routes */}
-                <Route path=":teamSlug" element={<AuthenticationGuardLayout />}>
-                  <Route
-                    element={
-                      <TeamProvider>
-                        <AuthorizationProvider>
-                          <RootLayout />
-                        </AuthorizationProvider>
-                      </TeamProvider>
-                    }
-                  >
-                    <Route index element={<Navigate to="boards" replace />} />
-                    <Route path="boards" element={<BoardsRoute />} />
-                    <Route path="members" element={<MembersRoute />} />
-                    <Route path="settings" element={<SettingsRoute />} />
-                  </Route>
-
-                  <Route
-                    path="boards/:boardId"
-                    element={
-                      <TeamProvider>
-                        <AuthorizationProvider>
+                <Route
+                  path="boards/:boardId"
+                  element={
+                    <TeamProvider>
+                      <AuthorizationProvider>
+                        <BoardContextProvider>
                           <BoardWizardLayout />
-                        </AuthorizationProvider>
-                      </TeamProvider>
-                    }
-                  >
-                    <Route index element={<BoardWizardRoute />} />
-                  </Route>
+                        </BoardContextProvider>
+                      </AuthorizationProvider>
+                    </TeamProvider>
+                  }
+                >
+                  <Route index element={<BoardWizardRoute />} />
                 </Route>
+              </Route>
 
-                <Route path="*" element={<ErrorRoute status={404} description="The page you are looking for does not exist." />} />
-              </Routes>
-            </div>
-          </BoardContextProvider>
+              <Route
+                path="*"
+                element={
+                  <ErrorRoute
+                    status={404}
+                    description="The page you are looking for does not exist."
+                  />
+                }
+              />
+            </Routes>
+          </div>
         </AuthenticationProvider>
       </ClientProvider>
     </QueryClientProvider>
--- apps/web/src/contexts/BoardContext/BoardContextProvider.tsx
+++ apps/web/src/contexts/BoardContext/BoardContextProvider.tsx
@@ -1,13 +1,13 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useState } from 'react'
 import { BoardController, BoardState } from '../../lib'
 import { BoardContext } from './BoardContext'
 
-export type BoardContextProvider = PropsWithChildren<{
-  boardState: BoardState
-  boardController: BoardController
-}>
+export type BoardContextProvider = PropsWithChildren
 
-export function BoardContextProvider ({ boardState, boardController, children }: BoardContextProvider) {
+export function BoardContextProvider ({ children }: BoardContextProvider) {
+  const [boardState] = useState(new BoardState())
+  const boardController = new BoardController(boardState)
+
   return (
     <BoardContext.Provider value={{ boardState, boardController }}>
       {children}
