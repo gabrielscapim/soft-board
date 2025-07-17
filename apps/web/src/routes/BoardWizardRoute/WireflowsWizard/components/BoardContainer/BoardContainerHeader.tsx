e71a5c4db5aa64363f7c8f980b3c0ff833e417a4
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-17 18:59:07 -0300

    Refactor BoardContainerHeader to improve layout and replace buttons with tooltip for maximize action

--- apps/web/src/routes/BoardWizardRoute/WireflowsWizard/components/BoardContainer/BoardContainerHeader.tsx
+++ apps/web/src/routes/BoardWizardRoute/WireflowsWizard/components/BoardContainer/BoardContainerHeader.tsx
@@ -1,25 +1,29 @@
 import { Button } from '@/components/ui/button'
-import { Maximize, PlayIcon } from 'lucide-react'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { Maximize2 } from 'lucide-react'
+import { Link } from 'react-router'
 
 export function BoardContainerHeader () {
   return (
-    <header className="bg-background sticky top-0 shrink-0 p-4">
+    <header className="bg-background sticky top-0 shrink-0 p-3">
       <div className="flex justify-between items-center w-full">
         <div className="flex items-center gap-2">
-          <Button
-            size="icon"
-            className="size-7"
-            variant="outline"
-          >
-            <Maximize />
-          </Button>
-          <Button
-            size="icon"
-            className="size-7"
-            variant="outline"
-          >
-            <PlayIcon />
-          </Button>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link to="edit">
+                <Button
+                  size="icon"
+                  className="size-7"
+                  variant="outline"
+                >
+                  <Maximize2 />
+                </Button>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>
+              Maximize Board
+            </TooltipContent>
+          </Tooltip>
         </div>
       </div>
     </header>
