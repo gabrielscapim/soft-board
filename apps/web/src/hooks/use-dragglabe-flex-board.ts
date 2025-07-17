ac552d1fafa521f3df03e42395be13984324bd4c
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-17 18:47:22 -0300

    Refactor useDraggableFlexBoard to include boardManager in DraggableBoard initialization and update event listener handling

--- apps/web/src/hooks/use-dragglabe-flex-board.ts
+++ apps/web/src/hooks/use-dragglabe-flex-board.ts
@@ -1,8 +1,9 @@
 import { useEffect } from 'react'
-import { BoardState, DraggableBoard } from '../lib'
+import { BoardManager, BoardState, DraggableBoard } from '../lib'
 
 export function useDraggableFlexBoard (
   boardState: BoardState,
+  boardManager: BoardManager,
   flexBoardRef: React.RefObject<HTMLDivElement>
 ) {
   useEffect(() => {
@@ -10,18 +11,20 @@ export function useDraggableFlexBoard (
 
     if (!element) return
 
-    const draggableBoard = new DraggableBoard(boardState, element)
+    const draggableBoard = new DraggableBoard({
+      boardElement: element,
+      boardState,
+      boardManager
+    })
 
     element.addEventListener('mousedown', draggableBoard.startDrag)
-    element.addEventListener('mousemove', draggableBoard.onDragging)
-    element.addEventListener('mouseup', draggableBoard.endDrag)
-    element.addEventListener('mouseleave', draggableBoard.endDrag)
+    window.addEventListener('mousemove', draggableBoard.onDragging)
+    window.addEventListener('mouseup', draggableBoard.endDrag)
 
     return () => {
       element.removeEventListener('mousedown', draggableBoard.startDrag)
-      element.removeEventListener('mousemove', draggableBoard.onDragging)
-      element.removeEventListener('mouseup', draggableBoard.endDrag)
-      element.removeEventListener('mouseleave', draggableBoard.endDrag)
+      window.removeEventListener('mousemove', draggableBoard.onDragging)
+      window.removeEventListener('mouseup', draggableBoard.endDrag)
     }
-  }, [flexBoardRef, boardState])
+  }, [flexBoardRef, boardState, boardManager])
 }
