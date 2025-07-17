9b94a40d2d119ecb94c5514b7633b05406c7cc69
Gabriel Scapim <scapimgabriel@gmail.com>
2025-07-17 18:59:11 -0300

    Refactor EditBoardRoute to integrate BoardPropertiesMenu and utilize useBoardContext for improved state management

--- apps/web/src/routes/EditBoardRoute/EditBoardRoute.tsx
+++ apps/web/src/routes/EditBoardRoute/EditBoardRoute.tsx
@@ -1,12 +1,27 @@
-import { Board } from '../../components'
-import { useBoard } from '../../hooks'
+import { Board, BoardPropertiesMenu } from '../../components'
+import { useBoardContext, useFlexComponents, useSelectedFlexComponents } from '../../hooks'
 
 export function EditBoardRoute () {
-  const { boardState, boardController } = useBoard()
+  const { boardState, boardController, boardManager } = useBoardContext()
+
+  const selected = useSelectedFlexComponents(boardState)
+  const flexComponents = useFlexComponents(boardState)
+  const selectedFlexComponents = flexComponents.filter(component => selected?.includes(component.id))
 
   return (
     <>
-      <Board boardState={boardState} boardController={boardController} />
+      {selectedFlexComponents.length > 0 && (
+        <BoardPropertiesMenu
+          boardState={boardState}
+          boardController={boardController}
+          selectedFlexComponents={selectedFlexComponents}
+        />
+      )}
+      <Board
+        boardState={boardState}
+        boardController={boardController}
+        boardManager={boardManager}
+      />
     </>
   )
 }
