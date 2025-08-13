061d60dc1f50d67d7b475068025f61aadd9c8848
Gabriel Scapim <scapimgabriel@gmail.com>
2025-08-12 21:26:09 -0300

    Create WireframeModeLink

---
+++ apps/web/src/components/WireframeModeLink/WireframeModeLink.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@/components/ui/button'
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { PlayIcon } from 'lucide-react'
+import { Link } from 'react-router'
+
+export type WireframeModeLinkProps = {
+  to: string
+}
+
+export function WireframeModeLink (props: WireframeModeLinkProps) {
+  const { to } = props
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Link to={to}>
+          <Button
+            size="icon"
+            className="size-7"
+            variant="outline"
+          >
+            <PlayIcon />
+          </Button>
+        </Link>
+      </TooltipTrigger>
+      <TooltipContent>
+        Wireframe Mode
+      </TooltipContent>
+    </Tooltip>
+
+  )
+}
