64fb664f7abaf1c7abd1941963a94e348501bf7b
Gabriel Scapim <scapimgabriel@gmail.com>
2025-10-09 23:45:34 -0300

    Fix variable naming for selected flex components in useSelectedFlexComponents hook

--- apps/web/src/components/Board/hooks/use-selected-flex-components.ts
+++ apps/web/src/components/Board/hooks/use-selected-flex-components.ts
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react'
 import { BoardState } from '../../../lib'
 
 export function useSelectedFlexComponents (boardState: BoardState) {
-  const [selectedFlexComponent, setSelectedFlexComponent] = useState(boardState.selectedFlexComponents)
+  const [selectedFlexComponents, setSelectedFlexComponents] = useState(boardState.selectedFlexComponents)
 
   useEffect(() => {
     const onChange = () => {
-      setSelectedFlexComponent(boardState.selectedFlexComponents)
+      setSelectedFlexComponents(boardState.selectedFlexComponents)
     }
 
     boardState.addListener('selectedFlexComponentsChanged', onChange)
@@ -16,5 +16,5 @@ export function useSelectedFlexComponents (boardState: BoardState) {
     }
   }, [boardState])
 
-  return selectedFlexComponent
+  return selectedFlexComponents
 }
