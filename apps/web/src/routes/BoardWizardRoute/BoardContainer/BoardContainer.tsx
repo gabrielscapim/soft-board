a60bad0f56975febea30684a2fb11253d31cc5a2
Gabriel Scapim <scapimgabriel@gmail.com>
2025-10-12 19:14:17 -0300

    Refactor BoardContainer to simplify props and remove unused hooks and components

--- apps/web/src/routes/BoardWizardRoute/BoardContainer/BoardContainer.tsx
+++ apps/web/src/routes/BoardWizardRoute/BoardContainer/BoardContainer.tsx
@@ -1,32 +1,11 @@
-import { Board, BoardPropertiesMenu, BoardProps, useFlexComponents, useSelectedFlexComponents } from '@/components'
-import { useBoard } from '@/hooks'
+import { Board, BoardProps } from '@/components'
 
-export type BoardContainerProps = Partial<BoardProps> & {
-  enableFullScreen?: boolean
-}
+export type BoardContainerProps = BoardProps
 
 export function BoardContainer (props: BoardContainerProps) {
-  const { boardState, boardController, boardManager } = useBoard()
-
-  const selected = useSelectedFlexComponents(boardState)
-  const flexComponents = useFlexComponents(boardState)
-  const selectedFlexComponents = flexComponents.filter(component => selected?.includes(component.id))
-
   return (
     <>
-      {selectedFlexComponents.length > 0 && props.enableSelection &&  (
-        <BoardPropertiesMenu
-          className="top-30"
-          boardState={boardState}
-          boardController={boardController}
-          flexComponents={flexComponents}
-          selectedFlexComponents={selectedFlexComponents}
-        />
-      )}
       <Board
-        boardState={boardState}
-        boardController={boardController}
-        boardManager={boardManager}
         {...props}
       />
     </>
