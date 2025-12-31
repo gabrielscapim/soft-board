a09ee7e7b1a06ef8a90631fefb77510012d388f5
Gabriel Scapim <scapimgabriel@gmail.com>
2025-12-30 21:43:48 -0300

    Conditionally render EditBoardLink based on board step in WireframeModeHeader

--- apps/web/src/components/WireframeModeHeader/WireframeModeHeader.tsx
+++ apps/web/src/components/WireframeModeHeader/WireframeModeHeader.tsx
@@ -1,12 +1,16 @@
+import { useBoard } from '@/hooks'
 import { BoardLink } from '../BoardLink'
 import { EditBoardLink } from '../EditBoardLink'
 
 export function WireframeModeHeader () {
+  const { board } = useBoard()
+
   return (
     <header className="bg-background sticky top-0 shrink-0 p-3 h-15 flex justify-between items-center w-full border-b-1">
       <div className="flex flex-row gap-2">
         <BoardLink to=".." />
-        <EditBoardLink to="../edit" />
+
+        {board?.step === 'wireflows' && <EditBoardLink to="../edit" />}
       </div>
     </header>
   )
