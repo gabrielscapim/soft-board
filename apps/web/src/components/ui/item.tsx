e3c0869757af613f03241d75c9ca3baab3ce9fc0
Gabriel Scapim <scapimgabriel@gmail.com>
2026-01-09 19:11:11 -0300

    Refactor Item component to use forwardRef for improved ref handling

--- apps/web/src/components/ui/item.tsx
+++ apps/web/src/components/ui/item.tsx
@@ -51,17 +51,14 @@ const itemVariants = cva(
   }
 )
 
-function Item ({
-  className,
-  variant = 'default',
-  size = 'default',
-  asChild = false,
-  ...props
-}: React.ComponentProps<'div'> &
-  VariantProps<typeof itemVariants> & { asChild?: boolean }) {
+const Item = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<'div'> & VariantProps<typeof itemVariants> & { asChild?: boolean }
+>(({ className, variant = 'default', size = 'default', asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : 'div'
   return (
     <Comp
+      ref={ref}
       data-slot="item"
       data-variant={variant}
       data-size={size}
@@ -69,7 +66,9 @@ function Item ({
       {...props}
     />
   )
-}
+})
+
+Item.displayName = 'Item'
 
 const itemMediaVariants = cva(
   'flex shrink-0 items-center justify-center gap-2 group-has-[[data-slot=item-description]]/item:self-start [&_svg]:pointer-events-none group-has-[[data-slot=item-description]]/item:translate-y-0.5',
