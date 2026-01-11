4f4be2b4752c3d4ada36cd1d9043cf2b084ddfcc
Gabriel Scapim <scapimgabriel@gmail.com>
2026-01-11 13:17:16 -0300

    Add SharedBoardContainer component for managing shared board state and display

---
+++ apps/web/src/routes/SharedBoardRoute/components/SharedBoardContainer/SharedBoardContainer.tsx
@@ -0,0 +1,66 @@
+import { Board, PreviewModeContainer, useBoardStore } from '@/components'
+import { BoardState, BoardManager, BoardController } from '@/lib'
+import { FlexComponent } from '@/types'
+import { useMemo, useState } from 'react'
+import { GetSharedBoardByTokenResult } from 'types/endpoints'
+import { SharedBoardHeader } from '../SharedBoardHeader'
+
+export type SharedBoardContainerProps = {
+  sharedBoard: GetSharedBoardByTokenResult
+}
+
+export function SharedBoardContainer (props: SharedBoardContainerProps) {
+  const { sharedBoard } = props
+
+  const [mode, setMode] = useState<'board' | 'preview'>('board')
+
+  const components = useMemo(() => {
+    return (
+      sharedBoard.board.components.map<FlexComponent>(component => ({
+        ...component,
+        type: component.type as FlexComponent['type'],
+        properties: component.properties as FlexComponent['properties']
+      }))
+    )
+  }, [sharedBoard.board.components])
+
+  const [board] = useState(() => {
+    const boardState = new BoardState({ id: sharedBoard.board.id, components })
+    const boardManager = new BoardManager({ client: null as any, boardState })
+    const boardController = new BoardController({ boardState, boardManager })
+
+    return { boardState, boardManager, boardController }
+  })
+
+  const scale = useBoardStore(board.boardState, 'scaleChanged', state => state.scale)
+
+  return (
+    <>
+      <SharedBoardHeader
+        board={sharedBoard.board}
+        scale={scale}
+        currentMode={mode}
+        onChangeMode={newMode => setMode(newMode)}
+        onChangeBoardScale={newScale => board.boardController.onChangeBoardScale({ scale: newScale })}
+      />
+
+      <main className="flex grow w-full h-[calc(100vh-60px)]">
+        {mode === 'board' && (
+          <Board
+            enableDraggable={false}
+            enableKeyboardShortcuts={false}
+            enableResizing={false}
+            enableSelection={false}
+            boardState={board.boardState}
+            boardController={board.boardController}
+            boardManager={board.boardManager}
+          />
+        )}
+
+        {mode === 'preview' && (
+          <PreviewModeContainer boardState={board.boardState} />
+        )}
+      </main>
+    </>
+  )
+}
