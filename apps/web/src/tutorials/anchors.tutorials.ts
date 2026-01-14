6909f5d9e01851606729525865ba8d2519a5af4a
Gabriel Scapim <scapimgabriel@gmail.com>
2026-01-14 14:25:15 -0300

    Refactor TUTORIALS_ANCHORS: reorganize and clean up entries for better clarity

--- apps/web/src/tutorials/anchors.tutorials.ts
+++ apps/web/src/tutorials/anchors.tutorials.ts
@@ -3,42 +3,37 @@
  * The keys represent the specific UI elements, and the values are the corresponding data-tutorial attributes.
  */
 export const TUTORIALS_ANCHORS = {
+  Board: 'board',
   BoardCard: 'board.list.item',
+  BoardComponentCardPreview: 'board.component.card.preview',
+  BoardPropertiesMenu: 'board.properties.menu',
+  BoardPropertiesMenuActionsTab: 'board.properties.menu.actions.tab',
+  BoardWizardChatContainer: 'board.chat.container',
+  BoardWizardFooter: 'board.footer',
+  BoardWizardFooterNextStepButton: 'board.footer.next',
+  BoardWizardHeaderSteps: 'board.header.steps',
+  BoardWizardRequirementsContainer: 'board.requirements.container',
+  BoardWizardRequirementsContainerCreateButton: 'board.requirements.container.create.button',
+  BoardWizardReviewContainer: 'board.review.container',
+  BoardWizardWireflowsContainer: 'board.wireflows.container',
+  BoardZoomController: 'board.zoom.controller',
   BoardsRoute: 'board.list.container',
+  CollapsibleEditBoardSidebar: 'board.sidebar.edit.collapsible',
   CreateBoardButton: 'board.list.create',
+  EditBoardHeaderAddScreenButton: 'board.header.add.screen.button',
+  EditBoardLink: 'board.edit.link',
+  InitWizardStepRequirements: 'board.init.step.requirements',
+  InitWizardStepReview: 'board.init.step.review',
+  InitWizardStepWireflows: 'board.init.step.wireflows',
+  NavUser: 'sidebar.user.menu',
+  PreviewModeContainerMobileScreenContainer: 'board.preview.mobile.screen.container',
+  PreviewModeLink: 'board.preview.mode.link',
+  PreviewModeMobileScreenContainerTopBarScreenSelector: 'board.preview.mobile.screen.selector',
   RootSidebar: 'sidebar.navigation',
   RootSidebarBoardsItem: 'sidebar.navigation.boards',
   RootSidebarMembersItem: 'sidebar.navigation.team',
   RootSidebarSettingsItem: 'sidebar.navigation.settings',
-  TeamSwitcher: 'sidebar.team.switcher',
-  NavUser: 'sidebar.user.menu',
-  BoardWizardHeaderSteps: 'board.header.steps',
-  BoardWizardFooter: 'board.footer',
-  InitWizardStepRequirements: 'board.init.step.requirements',
-  InitWizardStepWireflows: 'board.init.step.wireflows',
-  InitWizardStepReview: 'board.init.step.review',
-  BoardWizardFooterNextStepButton: 'board.footer.next',
-
-  BoardWizardRequirementsContainer: 'board.requirements.container',
-  BoardWizardRequirementsContainerCreateButton: 'board.requirements.container.create.button',
-  BoardWizardChatContainer: 'board.chat.container',
-  BoardWizardWireflowsContainer: 'board.wireflows.container',
-  BoardZoomController: 'board.zoom.controller',
-  PreviewModeLink: 'board.preview.mode.link',
-  EditBoardLink: 'board.edit.link',
-
-  Board: 'board',
-  CollapsibleEditBoardSidebar: 'board.sidebar.edit.collapsible',
-  BoardComponentCardPreview: 'board.component.card.preview',
-  BoardPropertiesMenu: 'board.properties.menu',
-  BoardPropertiesMenuActionsTab: 'board.properties.menu.actions.tab',
-
-  EditBoardHeaderAddScreenButton: 'board.header.add.screen.button',
-  PreviewModeContainerMobileScreenContainer: 'board.preview.mobile.screen.container',
-
-  BoardWizardReviewContainer: 'board.review.container',
-
-  PreviewModeMobileScreenContainerTopBarScreenSelector: 'board.preview.mobile.screen.selector'
+  TeamSwitcher: 'sidebar.team.switcher'
 }
 
 export function getPopoverAnchorSelector (anchorKey: keyof typeof TUTORIALS_ANCHORS) {
