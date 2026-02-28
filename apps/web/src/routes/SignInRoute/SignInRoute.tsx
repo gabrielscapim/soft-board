1b815eca1278d4d527b41cf4aa35f3558e7d22b2
Gabriel Scapim <scapimgabriel@gmail.com>
2026-02-28 16:04:23 -0300

    Optimize image loading in SignInRoute by using state for root image

--- apps/web/src/routes/SignInRoute/SignInRoute.tsx
+++ apps/web/src/routes/SignInRoute/SignInRoute.tsx
@@ -1,8 +1,11 @@
 import { Card, CardContent } from '@/components/ui/card'
 import { SignInForm } from './components'
 import { getRootImage } from '@/helpers'
+import { useState } from 'react'
 
 export function SignInRoute () {
+  const [rootImage] = useState(getRootImage())
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
@@ -12,7 +15,7 @@ export function SignInRoute () {
 
             <div className="bg-muted relative hidden md:block">
               <img
-                src={getRootImage()}
+                src={rootImage}
                 alt="Image"
                 className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
               />
