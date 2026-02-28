7568ed48cc38bd59de32d98070a2f130e60e195c
Gabriel Scapim <scapimgabriel@gmail.com>
2026-02-28 18:12:47 -0300

    Refactor getPool function to remove unnecessary pg variable check and ensure proper initialization

--- apps/api/src/libs/get-pool.ts
+++ apps/api/src/libs/get-pool.ts
@@ -2,19 +2,12 @@ import { Pool } from 'pg'
 import { DatabasePool } from 'pg-script'
 import { DATABASE_URL, DISABLE_REJECT_UNAUTHORIZED } from '../constants'
 
-let pg: DatabasePool | null = null
-
 export function getPool () {
-  if (pg) {
-    return pg
-  }
-
   const pool = new Pool({
     connectionString: DATABASE_URL,
     ...(DISABLE_REJECT_UNAUTHORIZED ? { ssl: { rejectUnauthorized: false } } : {})
   })
-
-  pg = new DatabasePool(pool)
+  const pg = new DatabasePool(pool)
 
   return pg
 }
